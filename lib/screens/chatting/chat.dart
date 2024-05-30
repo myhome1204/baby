0993748356e8a71b52882f05026c44d6a5c863db
jerry <3651471@naver.com>
2024-05-30 19:31:20 +0900

    채팅 화면 터치하면 키보드 사라지게 하는 기능 구현, 채팅 길이 길어지면 짤리는 현상 수정

--- lib/screens/chatting/chat.dart
+++ lib/screens/chatting/chat.dart
@@ -23,6 +23,8 @@ class ChatScreen extends StatefulWidget {
 class ChatScreenState extends State<ChatScreen> {
   final List<ChatMessage> _messages = <ChatMessage>[]; // 채팅 메시지를 저장하는 리스트
   final TextEditingController _textController = TextEditingController(); // 메시지 입력 필드의 텍스트를 제어하는 컨트롤러
+  final FocusNode _focusNode = FocusNode(); // FocusNode를 추가하여 포커스 제어
+  bool _isUserMessage = true; // 메시지가 사용자 메시지인지 여부를 나타내는 변수
 
   @override
   Widget build(BuildContext context) {
@@ -56,26 +58,31 @@ class ChatScreenState extends State<ChatScreen> {
           ),
         ),
       ),
-      body: Container(
-        color: Color(0xfff5deb3), // 배경색 지정
-        child: Column(
-          children: <Widget>[
-            Flexible(
-              child: ListView.builder(
-                padding: EdgeInsets.all(8.0),
-                reverse: true,
-                itemBuilder: (_, int index) => _messages[index],
-                itemCount: _messages.length,
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus(); // 화면 터치 시 포커스 해제.키보드 숨기기
+        },
+        child: Container(
+          color: Color(0xfff5deb3), // 배경색 지정
+          child: Column(
+            children: <Widget>[
+              Flexible(
+                child: ListView.builder(
+                  padding: EdgeInsets.all(8.0),
+                  reverse: true,
+                  itemBuilder: (_, int index) => _messages[index],
+                  itemCount: _messages.length,
+                ),
               ),
-            ),
-            Divider(height: 1.0),
-            Container(
-              decoration: BoxDecoration(
-                color: Theme.of(context).cardColor,
+              Divider(height: 1.0),
+              Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).cardColor,
+                ),
+                child: _buildTextComposer(),
               ),
-              child: _buildTextComposer(),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -95,6 +102,7 @@ class ChatScreenState extends State<ChatScreen> {
                 decoration: InputDecoration.collapsed(
                   hintText: "메세지를 입력하세요",
                 ),
+                focusNode: _focusNode, // FocusNode를 TextField에 추가.
               ),
             ),
             Container(
@@ -114,10 +122,11 @@ class ChatScreenState extends State<ChatScreen> {
     _textController.clear(); // 텍스트 필드 비우기.
     var message = ChatMessage(
       text: text,
-      isUserMessage: true, // 사용자 메시지로 설정,true면 사용자,false면 상대
+      isUserMessage: _isUserMessage, // 사용자 메시지 여부 설정
     );
     setState(() {
       _messages.insert(0, message); // 상태를 갱신하여 새로운 메시지를 리스트의 첫 번째 위치에 추가
+      _isUserMessage = !_isUserMessage; // 메시지를 보낼 때마다 isUserMessage 변수를 번갈아 변경
     });
   }
 }
@@ -140,27 +149,28 @@ class ChatMessage extends StatelessWidget {
               margin: const EdgeInsets.only(right: 16.0),
               child: CircleAvatar(child: Text(yourname[0])),
             ),
-          Column(
-            crossAxisAlignment: isUserMessage ? CrossAxisAlignment.end : CrossAxisAlignment.start, // 사용자 메시지는 오른쪽 정렬, 다른 메시지는 왼쪽 정렬
-            children: <Widget>[
-              if (!isUserMessage) // 사용자 메시지가 아닐 때만 이름 표시
-                Text(yourname, style: Theme.of(context).textTheme.headlineSmall),
-              Container(
-                margin: const EdgeInsets.only(top: 5.0),
-                padding: const EdgeInsets.all(10.0), // 메시지 박스의 패딩
-                decoration: BoxDecoration(
-                  color: isUserMessage ? Colors.red : Colors.white, // 사용자 메시지는 빨간색, 상대 메시지는 흰색 박스
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                child: Text(
-                  text,
-                  style: TextStyle(
-                    color: isUserMessage ? Colors.white : Colors.black, // 사용자 메시지는 흰색, 다른 메시지는 검은색
+          Expanded( // Expanded 위젯으로 감싸서 부모 위젯의 가로 공간을 모두 사용
+            child: Column(
+              crossAxisAlignment: isUserMessage ? CrossAxisAlignment.end : CrossAxisAlignment.start, // 사용자 메시지는 오른쪽 정렬, 다른 메시지는 왼쪽 정렬
+              children: <Widget>[
+                Text(isUserMessage ? _name : yourname, style: Theme.of(context).textTheme.headlineSmall),
+                Container(
+                  margin: const EdgeInsets.only(top: 5.0),
+                  padding: const EdgeInsets.all(10.0), // 메시지 박스의 패딩
+                  decoration: BoxDecoration(
+                    color: isUserMessage ? Colors.red : Colors.white, // 사용자 메시지는 빨간색, 상대 메시지는 흰색 박스
+                    borderRadius: BorderRadius.circular(8.0),
                   ),
-                  softWrap: true, // 텍스트 줄바꿈 설정
+                  child: Text(
+                    text,
+                    style: TextStyle(
+                      color: isUserMessage ? Colors.white : Colors.black, // 사용자 메시지는 흰색, 다른 메시지는 검은색
+                    ),
+                    softWrap: true, // 텍스트 줄바꿈 설정
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
           if (isUserMessage)
             Container(
